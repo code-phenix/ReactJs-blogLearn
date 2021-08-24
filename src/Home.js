6b0f4a45b3473397db0af59f895e8b3a1d0f0d20
unknown <sivashunmugasundaram007@gmail.com>
2021-08-24 19:26:30 +0530

    Loading effect

--- src/Home.js
+++ src/Home.js
@@ -7,6 +7,8 @@ const Home = () => {
     //      {author:'sss',title:'spring boot',id:3}
     //  ]);
     const [blogs,setblog]=useState(null);
+    const [isPending,setisPending]=useState(true);
+
      const handleDelete=(id) =>{
         const newBlog=blogs.filter((blog)=>blog.id!==id);
         setblog(newBlog);
@@ -17,12 +19,15 @@ const Home = () => {
             return res.json();
         }).then(data =>{
             setblog(data);
+            setisPending(false);
             console.log(data);
         })
      },[]);//this run for every renderi]85
     return (  
         <div className="home">
             {/* {blogs&&<BlogList blogs={blogs} title="Alll blogs" handleDelete={handleDelete}/>} */}
+            {isPending&&<div>Load aguthu.........</div>}
+            {/* it will used as loading screen when we try to fetch the data from anywhere */}
             {blogs&&<BlogList blogs={blogs} title="Alll blogs" />}
             {/* here initally the blogs is set to null , within fraction of second blog=null is mapped , here we are making it java scrip by putting Kali-braces and making with logical
             in java scrpit the logical operator will check the left , if left is true ,then right will excute */}
